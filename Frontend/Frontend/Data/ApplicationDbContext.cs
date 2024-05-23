007626e52e03ffb5a9ab71884e287b2ed980b2cd
Felix <felixsteen@gmail.com>
2024-05-23 09:23:06 +0200

    Subscribers notifications

--- Frontend/Frontend/Data/ApplicationDbContext.cs
+++ Frontend/Frontend/Data/ApplicationDbContext.cs
@@ -3,8 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Frontend.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
         public DbSet<AddressEntity> Addresses { get; set; }
+        public DbSet<SubscriberEntity> Subscribers { get; set; }
     }
 }
---
+++ Frontend/Frontend/Data/SubscriberEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Frontend.Data
+{
+    public class SubscriberEntity
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = null!;
+    }
+}
